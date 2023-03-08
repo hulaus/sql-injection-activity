2e090aad79b1d03d72ab71dab3ac6085789e56c5
Jo Carr <hulaus21@gmail.com>
2023-03-07 23:13:06 -0800

    initial commit

--- app.js
+++ app.js
@@ -17,43 +17,31 @@ db.serialize(function () {
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('/index.html');
 });
 
 app.post('/login', function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    const query = "SELECT title FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
+    var username = req.body.username;
+    var password = req.body.password;
+    var query = "SELECT title FROM user WHERE username = '" + username + "' and password = '" + password + "'";
+    
+    console.log("username:" + username);
+    console.log("password:" + password);
+    console.log('query: ' + query);
+
     db.get(query, function(err, row) {
-        if (err) {
-            console.error(err.message);
-        } else if (row) {
-            console.log(row.title);
-        } else {
-            console.log('Invalid login credentials');
-        }
-    });
-});
-
-app.post('/login', function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log('Username:', username);
-    console.log('Password:', password);
-    const query = "SELECT title FROM user WHERE username ='" + username + " ' AND password = '" + password + "'";
-    console.log('Query:', query);
-
-    db.get(query, function (err, row) {
         if (err) {
             console.log('ERROR', err);
             res.redirect("/index.html#error");
         } else if (!row) {
             res.redirect("/index.html#unauthorized");
         } else {
-            res.send('Hello <b>' + row.title + '!</b><br />This file contains all your secret data: <br /><br />SECRETS <br /><br />MORE SECRETS <br /><br /><a href="/index.html">Go back to login</a>');
-        }
-    });
+            res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
+	});
+
 });
+
 
 app.listen(4001, function () {
     console.log('Server is listening on Port 4001!')
